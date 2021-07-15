f85f6ea9fab245686cb399f1d2c418abbb6ce8cd
vbeaulieu_adops <vbeaulieu@eudonet.ca>
2021-07-15 17:09:30 -0400

    ajout icones et aspect ratio

--- app.js
+++ app.js
@@ -11,98 +11,111 @@ new Vue({
         tiles: [],
         lives: 0,
     },
-    created(){
+    created() {
         this.newGame()
     },
     computed: {
-        flags(){
+        flags() {
             return this.tiles.flat().filter(t => t.isFlag).length
         },
-        opened(){
+        opened() {
             return this.tiles.flat().filter(t => t.isOpen).length
         },
+        openedBombs() {
+            return this.tiles.flat().filter(t => t.isOpen && t.isBomb).length
+        },
+        minesLeft() {
+            return this.options.mines - this.flags - this.openedBombs
+        }
     },
-    methods:{
-        newGame(){
+    methods: {
+        newGame() {
             this.tiles = []
-            this.lives=this.options.lives
-            for(let i = 0; i < this.options.h; i++){
+            this.lives = this.options.lives
+            for (let i = 0; i < this.options.h; i++) {
                 let row = []
-                for(let j = 0 ; j < this.options.w; j++){
+                for (let j = 0; j < this.options.w; j++) {
                     row.push({
-                        x:j, y:i, isBomb: false, isFlag: false, isOpen:false, bombs: 0
+                        x: j, y: i, isBomb: false, isFlag: false, isOpen: false, bombs: 0
                     })
                 }
                 this.tiles.push(row)
             }
 
             let placed = 0
-            while( placed < this.options.mines){
-                let y = Math.floor(Math.random()  * this.options.h)
+            while (placed < this.options.mines) {
+                let y = Math.floor(Math.random() * this.options.h)
                 let x = Math.floor(Math.random() * this.options.w)
-                if(!this.tiles[y][x].isBomb){
+                if (!this.tiles[y][x].isBomb) {
                     this.tiles[y][x].isBomb = true
                     placed++
                 }
             }
-            for(let i = 0; i < this.options.h; i++){
-                for(let j = 0 ; j < this.options.w; j++){
-                    if(!this.tiles[i][j].isBomb){
+            for (let i = 0; i < this.options.h; i++) {
+                for (let j = 0; j < this.options.w; j++) {
+                    if (!this.tiles[i][j].isBomb) {
                         this.tiles[i][j].bombs = this.bombAround(j, i)
                     }
                 }
             }
 
         },
-        bombAround(x, y){
+        bombAround(x, y) {
             let bombs = 0
             let tilesAround = this.tilesAround(x, y)
-            for(let i = 0; i < tilesAround.length; i++){
-                if(this.tiles[tilesAround[i].y][tilesAround[i].x].isBomb){
+            for (let i = 0; i < tilesAround.length; i++) {
+                if (this.tiles[tilesAround[i].y][tilesAround[i].x].isBomb) {
                     bombs++
                 }
             }
             return bombs
         },
-        tilesAround(x, y){
+        tilesAround(x, y) {
             let tiles = []
-            if(x > 0){ tiles.push({y, x: x-1}) }
-            if(y > 0){ tiles.push({y: y-1, x}) }
-            if(x < this.options.w - 1){ tiles.push({y, x: x+1}) }
-            if(y < this.options.h - 1){ tiles.push({y: y+1, x: x}) }
-            if(x > 0 && y > 0){ tiles.push({y: y-1, x: x-1}) }
-            if(x > 0 && y < this.options.h - 1){ tiles.push({y: y+1, x: x-1}) }
-            if(x < this.options.w - 1 && y > 0){ tiles.push({y: y-1, x: x+1}) }
-            if(x < this.options.w - 1 && y < this.options.h - 1){ tiles.push({y: y+1, x: x+1}) }
+            if (x > 0) { tiles.push({ y, x: x - 1 }) }
+            if (y > 0) { tiles.push({ y: y - 1, x }) }
+            if (x < this.options.w - 1) { tiles.push({ y, x: x + 1 }) }
+            if (y < this.options.h - 1) { tiles.push({ y: y + 1, x: x }) }
+            if (x > 0 && y > 0) { tiles.push({ y: y - 1, x: x - 1 }) }
+            if (x > 0 && y < this.options.h - 1) { tiles.push({ y: y + 1, x: x - 1 }) }
+            if (x < this.options.w - 1 && y > 0) { tiles.push({ y: y - 1, x: x + 1 }) }
+            if (x < this.options.w - 1 && y < this.options.h - 1) { tiles.push({ y: y + 1, x: x + 1 }) }
             return tiles;
         },
-        leftClick(x, y){
-            this.openTile(x,y, [])
+        leftClick(x, y) {
+
+            if (!this.tiles[y][x].isOpen) {
+                this.openTile(x, y, [])
+            }
         },
-        rightClick(x, y){
-            if(!this.tiles[y][x].isOpen){
+        rightClick(x, y) {
+            if (!this.tiles[y][x].isOpen) {
                 this.tiles[y][x].isFlag = !this.tiles[y][x].isFlag
             }
         },
-        openTile(x, y, opened){
-            if(this.tiles[y][x].isBomb){
+        openTile(x, y, opened) {
+            if (this.tiles[y][x].isFlag) {
+                return
+            }
+            if (this.tiles[y][x].isBomb) {
+                this.tiles[y][x].isOpen = true
                 console.log("You exploded !")
                 this.lives--
                 return
             }
             this.tiles[y][x].isOpen = true
-            opened.push({x, y})
-            if(this.tiles[y][x].bombs == 0){
+            opened.push({ x, y })
+            if (this.tiles[y][x].bombs == 0) {
                 let tilesAround = this.tilesAround(x, y)
-                for(let i = 0; i < tilesAround.length; i++){
-                    if(opened.find(c => c.x == tilesAround[i].x && c.y == tilesAround[i].y) == null){
-                        this.openTile(tilesAround[i].x,tilesAround[i].y, opened )
+                for (let i = 0; i < tilesAround.length; i++) {
+                    if (opened.find(c => c.x == tilesAround[i].x && c.y == tilesAround[i].y) == null) {
+                        this.openTile(tilesAround[i].x, tilesAround[i].y, opened)
                     }
                 }
             }
-            
+
 
         }
-        
+
     }
 })
